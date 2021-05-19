cb5974f9fd4cbb8fd84bdf7d8554025e8bfa7716
Grape Moshe <gmoshe@at-m7.msk.21-school.ru>
2021-05-19 19:21:33 +0300

    parser interface

--- sources/config_parser/config_parser.h
+++ sources/config_parser/config_parser.h
@@ -8,8 +8,45 @@
 #include <map>
 #include <cstdio>
 
-std::vector<std::string> FileReading(std::string file_name);
-std::map<std::string, std::map<std::string, std::string> > Parser(std::vector<std::string> data);
-std::map<std::string, std::string> ParserCategory(std::vector<std::string>, size_t start);
+std::vector<std::string> stringSplit(std::string str, char sign);
+
+namespace ft {
+
+class config
+{
+ private:
+
+	struct __interior
+	{
+		std::map<std::string, std::string> fields;
+		std::map<std::string, __interior> subcategory;
+	};
+
+	std::map<std::string, __interior> __category;
+
+	void pushSubcategory(std::vector<std::string> conf, size_t count);
+
+ public:
+
+	typedef __interior							categoryObject;
+	typedef std::map<std::string, __interior>	categoryType;
+
+	config(std::string __fileName);
+	config(config const &other);
+
+	categoryObject				getCategoryObject(std::string category);
+	categoryObject				getCategoryObject(categoryObject object, std::string category);
+
+	std::vector<std::string>	getCategoryInteriorFields(std::string category); // category("logger/http")
+	std::vector<std::string>	getCategoryInteriorFields(categoryObject object, std::string category);
+
+	std::vector<std::string>	getSubcategory(std::string category);
+	std::vector<std::string>	getSubcategory(categoryObject object, std::string category);
+
+	std::vector<std::string>	getCategory();
+	std::string					fieldData(categoryObject const &objeck, std::string field);
+};
+
+};
 
 #endif
