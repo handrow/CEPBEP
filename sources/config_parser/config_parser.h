87e7745789bda9f15cc5032fa19a1c36a5e98be1
Grape Moshe <gmoshe@at-m7.msk.21-school.ru>
2021-05-22 19:00:36 +0300

    interface change

--- sources/config_parser/config_parser.h
+++ sources/config_parser/config_parser.h
@@ -4,13 +4,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 #include <fstream>
 #include <map>
 #include <cstdio>
 
-std::vector<std::string> stringSplit(std::string str, char sign);
-
 namespace ft {
+
+std::vector<std::string> stringSplit(std::string str, char sign);
+void trim(std::string &str);
 
 class config
 {
@@ -19,32 +21,38 @@ class config
 	struct __interior
 	{
 		std::map<std::string, std::string> fields;
-		std::map<std::string, __interior> subcategory;
+		std::map<std::string, __interior*> subcategory;
 	};
 
-	std::map<std::string, __interior> __category;
+	std::map<std::string, __interior*> __category;
 
+	std::vector<std::string> FileReading(std::string file_name);
+	std::map<std::string, std::string> ParserCategory(std::vector<std::string> config, size_t start);
 	void pushSubcategory(std::vector<std::string> conf, size_t count);
 
  public:
 
 	typedef __interior							categoryObject;
-	typedef std::map<std::string, __interior>	categoryType;
+	typedef std::map<std::string, __interior*>	categoryType;
+	typedef std::map<std::string, std::string>	fieldsType;
 
 	config(std::string __fileName);
 	config(config const &other);
 
-	categoryObject				getCategoryObject(std::string category);
-	categoryObject				getCategoryObject(categoryObject object, std::string category);
-
-	std::vector<std::string>	getCategoryInteriorFields(std::string category); // category("logger/http")
-	std::vector<std::string>	getCategoryInteriorFields(categoryObject object, std::string category);
-
-	std::vector<std::string>	getSubcategory(std::string category);
-	std::vector<std::string>	getSubcategory(categoryObject object, std::string category);
-
 	std::vector<std::string>	getCategory();
-	std::string					fieldData(categoryObject const &objeck, std::string field);
+	categoryObject				*getCategoryObject(std::string &category);
+	categoryObject				*getCategoryObject(categoryObject *object, std::string &category);
+
+	std::vector<std::string>	getCategoryInteriorFields(std::string &category); // category("logger:http")
+	std::vector<std::string>	getCategoryInteriorFields(categoryObject *object);
+	std::vector<std::string>	getCategoryInteriorFields(categoryObject *object, std::string &category);
+
+	std::vector<std::string>	getSubcategory(std::string &category);
+	std::vector<std::string>	getSubcategory(categoryObject *object);
+	std::vector<std::string>	getSubcategory(categoryObject *object, std::string &category);
+
+	std::string					fieldData(std::string &category,  std::string &field);
+	std::string					fieldData(categoryObject *objeck, std::string &field);
 };
 
 };
