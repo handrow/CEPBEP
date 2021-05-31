0e0e893cc77d40305885d1be2476813c9002731c
Grape Moshe <gmoshe@at-m7.msk.21-school.ru>
2021-05-31 18:05:14 +0300

    new interface config_parser

--- sources/config_parser/config_parser.h
+++ sources/config_parser/config_parser.h
@@ -8,48 +8,82 @@
 #include <map>
 #include <cstdio>
 
-namespace ft {
-
-std::vector<std::string> stringSplit(std::string str, char sign);
-void trim(std::string &str);
+namespace Ft {
+namespace ConfigParser {
 
 class Config {
+
  private:
-    struct __interior {
-        std::map<std::string, std::string> fields;
-        std::map<std::string, __interior*> subcategory;
-    };
-
-    std::map<std::string, __interior*> __category;
-
-    std::vector<std::string>            FileReading(std::string file_name);
-    std::map<std::string, std::string>  ParserCategory(std::vector<std::string> config, size_t start);
-    void                                pushSubcategory(std::vector<std::string> conf, size_t count);
+    std::string&                        __name;
+    std::map<std::string, std::string> __fields;
+    std::map<std::string, Config>      __categories;
 
  public:
-    typedef __interior                            categoryObject;
-    typedef std::map<std::string, __interior*>    categoryType;
-    typedef std::map<std::string, std::string>    fieldsType;
+    typedef Config                                Category;
+    typedef std::pair<std::string, std::string>   Field;
+    typedef std::map<std::string, std::string>    FieldType;
+    typedef std::map<std::string, Config>         CategoryType;
 
-    explicit Config(std::string __fileName);
-    explicit Config(Config const &other);
+    explicit Config();
+	~Config();
+    Config &operator=(const Config &other);
 
-    std::vector<std::string>    getCategory();
-    categoryObject              *getCategoryObject(std::string const &category);
-    categoryObject              *getCategoryObject(categoryObject *object, std::string const &category);
+    FieldType::iterator             GetFieldIterBegin();
+    CategoryType::iterator        GetCategoryIterBegin();
 
-    std::vector<std::string>    getCategoryInteriorFields(std::string const &category);  // category("logger:http")
-    std::vector<std::string>    getCategoryInteriorFields(categoryObject *object);
-    std::vector<std::string>    getCategoryInteriorFields(categoryObject *object, std::string const &category);
+    void                        SetField(const std::string &key, const std::string &value);
+    void                        RemoveField(const std::string &key);
 
-    std::vector<std::string>    getSubcategory(std::string const &category);
-    std::vector<std::string>    getSubcategory(categoryObject *object);
-    std::vector<std::string>    getSubcategory(categoryObject *object, std::string const &category);
+    Category                    Category(const std::string &key);
+    bool                        HasCategory(const std::string &key);
+    bool                        HasValue(const std::string &key);
 
-    std::string                  fieldData(std::string const &category,  std::string const &field);
-    std::string                  fieldData(categoryObject *objeck, std::string const &field);
+    Field                       Field(const std::string &key);
+    Field::second_type          &FieldValue(Field &field);
+    const Field::first_type     &FieldKey(Field &field);
+
+    std::string                 &name();
+    void                        AddField();
+    void                        AddCategory();
+    void                        RemoveCategory();
+    size_t                      CountFields();
+    size_t                      CountCategories();
 };
 
-};  // namespace ft
+void    dump_to_ini(Config conf, std::string path);
+Config  parse_from_ini(const std::string &__fileName);
+
+class FieldIterator {
+ private:
+    Config::FieldType::iterator __ptr;
+ public:
+    Config::Field               &operator*();
+    FieldIterator               &operator=(const FieldIterator &other);
+    FieldIterator               &operator=(const Config::FieldType::iterator &other);
+    std::string                 &Value();
+    const std::string           &Key();
+    void                        Next();
+    void                        Prev();
+    bool                        IsEnd();
+    bool                        IsRend();
+};
+
+class CategoryIterator {
+ private:
+    Config::CategoryType::iterator __ptr;
+ public:
+    Config::Category            &operator*();
+    CategoryIterator            &operator=(const CategoryIterator &other);
+    CategoryIterator            &operator=(const Config::CategoryType::iterator &other);
+    std::string                 &Name();
+    void                        Next();
+    void                        Prev();
+    bool                        IsEnd();
+    bool                        IsRend();
+};
+
+};  // namespace Ft
+
+};  // namespace Config_parser
 
 #endif  // CONFIG_PARSER_CONFIG_PARSER_H_
