3d89ba8c951dcbe920e010920af91a96f12aa011
mcottonm <73592455+mcottonm@users.noreply.github.com>
2021-06-19 12:34:43 +0300

    [NETLIB] add socket (#9)
    Co-authored-by: Headless Granules <hgranule@am-i3.msk.21-school.ru>
    Co-authored-by: Merrett Cottonmouth <mcottonm@at-m7.msk.21-school.ru>

---
+++ sources/netlib/socket.cc
@@ -0,0 +1,145 @@
+#include "socket.h"
+
+namespace Netlib {
+
+// IPADDRV4
+
+IpAddrV4::IpAddrV4(u32 addr) {
+    __val.raw = htonl(addr);
+}
+
+IpAddrV4::IpAddrV4(const std::string& str) {
+    __val.raw = inet_addr(str.c_str());
+}
+
+IpAddrV4::operator std::string() const {
+    struct in_addr l;
+    l.s_addr = __val.raw;
+    return inet_ntoa(l);
+}
+
+IpAddrV4::operator u32() const {
+    return ntohl(__val.raw);
+}
+
+// PORT
+
+Port::Port(u16 addr) {
+    __val.raw = htons(addr);
+}
+
+Port::Port(const std::string& str) {
+    u16 i = atoi(str.c_str());
+    __val.raw = htons(i);
+}
+
+Port::operator std::string() const {
+    return std::to_string(ntohs(__val.raw));
+}
+
+Port::operator u16() const {
+    return ntohs(__val.raw);
+}
+
+// SOCKIONFO
+
+SockInfo::SockInfo(IpAddrV4 ip, Port port)
+: addr_BE(ip)
+, port_BE(port) {
+}
+
+SockInfo::SockInfo(const sockaddr_in& sin)
+: addr_BE(sin.sin_addr.s_addr)
+, port_BE(sin.sin_port) {
+}
+
+SockInfo::operator sockaddr_in() const {
+    sockaddr_in saddr;
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = port_BE.__val.raw;
+    saddr.sin_addr.s_addr = addr_BE.__val.raw;
+    return saddr;
+}
+
+// SOCKET
+
+Socket::Socket(const SockInfo& sinfo, fd_t fd) : __info(sinfo), __fd(fd) {}
+
+Socket Socket::CreateListenSocket(const SockInfo& sinfo, Error* err) {
+    Socket             sock;
+    int                rc;
+
+    rc = socket(AF_INET, SOCK_STREAM, 0);
+    if (rc < 0)
+        return *err = SystemError(errno), sock;
+    sock.__fd = rc;
+
+    sockaddr_in addr = sinfo;
+    rc = bind(sock.__fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+    if (rc < 0)
+        return *err = SystemError(errno), sock;
+    sock.__info = sinfo;
+
+    rc = listen(sock.__fd, 0);
+    if (rc < 0)
+        return *err = SystemError(errno), sock;
+
+    return sock;
+}
+
+Socket Socket::AcceptNewConnection(Socket* listen_sock, Error* err) {
+    SockInfo    sinfo;
+    int         rc;
+
+    socklen_t   slen;
+    sockaddr    sa;
+    rc = accept(listen_sock->__fd, &sa, &slen);
+
+    if (rc < 0)
+        *err = SystemError(errno);
+    else if (sa.sa_family != AF_INET)
+        *err = Error(NET_BAD_FAMILY_ERR, "Not AF_INET connection");
+    else
+        sinfo = SockInfo(*reinterpret_cast<sockaddr_in *>(&sa));
+
+    return Socket(sinfo, rc);
+}
+
+void    Socket::AddFileFlag(int flag, Error* err) {
+    int flags = fcntl(__fd, F_GETFL);
+    if (flags < 0)
+        *err = SystemError(errno);
+    else if (fcntl(__fd, F_SETFL, flags | flag) < 0)
+        *err = SystemError(errno);
+}
+
+void    Socket::Close() {
+    close(__fd);
+}
+
+fd_t    Socket::GetFd() {
+    return __fd;
+}
+
+std::string Socket::Read(usize nbytes, Error* err) {
+    isize rc;
+    std::string s;
+
+    s.reserve(nbytes);
+
+    rc = read(__fd, const_cast<char*>(s.data()), nbytes);
+    if (rc < 0)
+        *err = Error(NET_SYSTEM_ERR, "Syscall error: read");
+    s.resize(rc);
+    return s;
+}
+
+isize   Socket::Write(const std::string& s, Error* err) {
+    isize rc = write(__fd, const_cast<char*>(s.data()), s.size());
+
+    if (rc < 0)
+        *err = Error(NET_SYSTEM_ERR, "Syscall error: write");
+    return rc;
+}
+
+}  // namespace Netlib
---
+++ sources/netlib/socket.h
@@ -0,0 +1,85 @@
+#ifndef NETLIB_SOCKET_H_
+#define NETLIB_SOCKET_H_
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <string>
+#include <cerrno>
+#include <fcntl.h>
+
+#include "../common/types.h"
+#include "../common/error.h"
+
+namespace Netlib {
+
+enum NetLibErrorCode {
+
+    NET_NO_ERR = 10000,
+    NET_SYSTEM_ERR,
+    NET_BAD_FAMILY_ERR
+};
+
+// IpAddrV4(0x7f000001) -> [0x7f, 0x0, 0x0, 0x1]
+// IpAddrV4("127.0.0.1") -> [0x7f, 0x0, 0x0, 0x1]
+// std::string(ipaddr) -> "127.0.0.1"
+// u32(ipaddr) -> 0x7f000001
+
+struct IpAddrV4 {
+    union {
+        u32 raw;
+        u8  bytes[4];
+    } __val;
+
+             IpAddrV4(u32 addr);                // NOLINT(*)
+             IpAddrV4(const std::string& str);  // NOLINT(*)
+    operator std::string() const;
+    operator u32() const;
+};
+
+struct Port {
+    union {
+        u16 raw;
+        u8  bytes[2];
+    } __val;
+
+             Port(u16 addr);                    // NOLINT(*)
+             Port(const std::string& str);      // NOLINT(*)
+    operator std::string() const;
+    operator u16() const;
+
+};
+
+struct SockInfo {
+    IpAddrV4    addr_BE;
+    Port        port_BE;
+
+    explicit SockInfo(IpAddrV4 ip = u32(-1), Port port = u16(-1));
+    explicit SockInfo(const sockaddr_in& sin);
+    operator struct sockaddr_in() const;
+};
+
+class Socket {
+ protected:
+    SockInfo  __info;
+    fd_t      __fd;
+
+ public:
+    explicit Socket(const SockInfo& sinfo = SockInfo(), fd_t fd = -1);
+
+    static Socket CreateListenSocket(const SockInfo& info, Error* err);
+    static Socket AcceptNewConnection(Socket* listen_sock, Error* err);
+
+    void        AddFileFlag(int flag, Error* err);
+    void        Close();
+    fd_t        GetFd();
+    std::string Read(usize nbytes, Error* err);
+    isize       Write(const std::string& s, Error* err);
+
+};
+
+}  // namespace Netlib
+
+#endif  // NETLIB_SOCKET_H_
